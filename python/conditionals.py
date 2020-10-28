3a1b6d174731b12a070991bf52a3e88ad150a65d
robq <robq71@gmail.com>
2020-10-28 17:26:16 +0000

    using a while loop

---
+++ python/conditionals.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 12:46:10 2020
+
+@author: robq
+"""
+
+# x = float(input("Enter a num for x: "))
+# y = float(input("Enter a num for y: "))
+# if x == y:
+#     print("x and y are equal!")
+#     if y != 0:
+#         print("Therefore, x / y is", x/y)
+# elif x < y:
+#     print("\nx is a smaller number")
+# else:
+#     print("\ny is a smaller number")
+    
+# print("\nThanks Yo!")
+
+
+n = input("You are still in the the Lost Forest\nGo left (l) or right (r) ? ")
+while n == "r" or n == "R":
+    n = input("You are still in the Lost Forest\nGo left (l) or right (r) ? ")
+
+print("\nYou made it out of the Lost Forest!\n\n              \o/")
