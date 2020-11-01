aa487b6f190e9ca188f6345ce426b3cff3bbc5a2
robq <robq71@gmail.com>
2020-11-01 20:37:14 +0000

    updated binary_search with output

--- python/extras/binary_search.py
+++ python/extras/binary_search.py
@@ -9,30 +9,44 @@ Created on Sat Oct 31 11:05:36 2020
 # Taken from https://www.youtube.com/watch?v=zeULw-a7Mw8
 
 
-data = [2,4,5,7,8,9,12,14,17,19,22,25,27,28,33,37]
-target = 25
+data = [2,4,5,7,8,9,12,14,17,19,22,25,27,28,33,37,40,42,45,49]
+target = 37
+
+print("data list =",data)
+print("target =",target)
+print("\r")
+
 
 # Linear Search
 def linear_search(data, target):
     for i in range(len(data)):
         if data[i] == target:
+            print("The target in the list was",data[i])
             return True
+    print("The target no.",target,"was not found in the list!")    
     return False
+
 
 # Iterative Binary Search
 def binary_search_iterative(data, target):
     low = 0
-    high = len(data) - 1
+    high = len(data)-1
     
+    print("low is =",low)
+    print("high = len(data)-1 which is =",high)
+        
     while low <= high:
         mid = (low + high) // 2
         if target == data[mid]:
+            print("mid = (low + high) // 2")
+            print("The element is =",data[mid])
             return True
         elif target < data[mid]:
             high = mid - 1
         else:
             low = mid + 1
     return False
+
 
 # Recursive Binary Search
 def binary_search_recursive(data, target, low, high):
@@ -41,11 +55,17 @@ def binary_search_recursive(data, target, low, high):
     else:
         mid = (low + high) // 2
         if target == data[mid]:
+            print("The data[mid] is =",data[mid])
             return True
         elif target < data[mid]:
             return binary_search_recursive(data, target, low, mid-1)
         else:
             return binary_search_recursive(data, target, mid+1, high)
 
-print(binary_search_iterative(data, target))
-print(binary_search_recursive(data, target, 0, len(data)-1))
+
+print("linear_search outcome:",linear_search(data, target))
+print("\r")
+print("binary_search_iterative outcome:",binary_search_iterative(data, target))
+print("\r")
+print("binary_search_recursive outcome:",binary_search_recursive(data, target, 0, len(data)-1))
+
